d9dcd08e3255ff25b1c3e41cd657c967dfafb5e1
Moustache <3254266353@qq.com>
2018-01-22 04:06:11 -0500

    debug

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import pandas
 
 # FROM:xjm
 #firsttable->array1  secondtable->array2
-array1, array2 = GetTwoTable() 
+array1, array2 = spider.GetTwoTable() 
 
-table1_dataframe, table1_sort_1, table1_sort_2, table2_dataframe  = deal_func(array1, array2)
+table1_dataframe, table1_sort_1, table1_sort_2, table2_dataframe  = spider.deal_func(array1, array2)
 
 table1_dataframe.to_csv('table_one.csv')
 table2_dataframe.to_csv('table_second.csv')
--- spider.py
+++ spider.py
@@ -3,6 +3,7 @@ import numpy as np
 import re
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 """
 FROM:hza
