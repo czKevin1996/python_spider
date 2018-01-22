e0f11d4102fcb569f55674f13dd52c2a02033cfa
Moustache <3254266353@qq.com>
2018-01-22 17:01:23 +0800

    final merge

---
+++ main.py
@@ -0,0 +1,18 @@
+import spider
+import pandas
+
+# FROM:xjm
+#firsttable->array1  secondtable->array2
+array1, array2 = GetTwoTable() 
+
+table1_dataframe, table1_sort_1, table1_sort_2, table2_dataframe  = deal_func(array1, array2)
+
+table1_dataframe.to_csv('table_one.csv')
+table2_dataframe.to_csv('table_second.csv')
+table1_sort_1.to_csv('ups_and_downs.csv')
+table1_sort_2.to_csv('VOL.csv')
+
+print table1_dataframe
+print table2_dataframe
+print table1_sort_1
+print table1_sort_2 
